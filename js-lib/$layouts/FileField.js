511bffcb0d44d8f6f3cdbfc773da786630bc2514
SfTd <j.zolcik@allblue.pl>
2020-07-23 11:23:54 +0200

    Development.

--- js-lib/$layouts/FileField.js
+++ js-lib/$layouts/FileField.js
@@ -7,7 +7,7 @@ const
 export default class FileField extends spocky.Layout {
 
     static get Content() {
-        return [["div",{"class":["form-group {{divClass}} {{fullFieldName}}_Validator.divClass"],"_data-spk-field":["{{formName}}:{{fieldInfo}}"]},["label",{"_show":["{{fullFieldName}}_Label"],"for":["{{fullFieldName}}"],"class":["{{labelClass}}"]},"{{fieldLabel}}"],["div",{"class":["{{field.fieldClass}}"]},["input",{"_elem":["{{fullFieldName}}_Field"],"type":["file"],"accept":["${{fullFieldName}}_Accept"],"id":["{{fullFieldName}}"],"name":["{{fieldName}}"],"class":["form-control ${{fullFieldName}}_Validator.fieldClass"]}],["p",{"_repeat":["{{fullFieldName}}_Validator.errors:errorMessage"],"class":["invalid-feedback"]},"$errorMessage"]]]];
+        return [["div",{"class":["form-group {{divClass}} {{fullFieldName}}_Validator.divClass"],"_data-spk-field":["{{formName}}:{{fieldInfo}}"]},["label",{"_show":["{{fullFieldName}}_Label"],"for":["{{fullFieldName}}"],"class":["{{labelClass}}"]},"{{fieldLabel}}"],["div",{"class":["{{field.fieldClass}}"]},["input",{"_elem":["{{fullFieldName}}_Field"],"type":["file"],"accept":["$fullFieldName","}_Accept"],"id":["{{fullFieldName}}"],"name":["{{fieldName}}"],"class":["form-control ","$fullFieldName","}_Validator.fieldClass"]}],["p",{"_repeat":["{{fullFieldName}}_Validator.errors:errorMessage"],"class":["invalid-feedback"]},"$errorMessage"]]]];
     }
 
 
